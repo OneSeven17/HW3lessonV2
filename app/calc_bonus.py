00a7b53f147783dfb007fad9ccea0fd00e21f5fc
OneSeven17 <17renatis@gmail.com>
2019-02-17 18:37:01 +0300

    Late initial commit

---
+++ app/calc_bonus.py
@@ -0,0 +1,35 @@
+def calculate_bonuses (the_sum_of_current_purchase):
+    """
+        calculate_bonuses(900)
+        0...
+        calculate_bonuses(1_000)
+        50...
+        calculate_bonuses(16_000)
+        1120...
+        calculate_bonuses(150_000)
+        15000...
+    """
+    the_sum_of_previous_purchases = 0
+    blue_card_percent = 0.05
+    silver_card_percent = 0.07
+    gold_card_percent = 0.1
+    magic_number = 1000 #бонусы начисляются за каждые 1000 руб, поэтому вводим данную переменную
+    the_sum_of_all_purchases = the_sum_of_previous_purchases + the_sum_of_current_purchase
+
+    if the_sum_of_all_purchases <1_000:
+        bonus_for_purchase = 0
+    if 1_000 <= the_sum_of_all_purchases <= 15_000:
+        bonus_for_purchase = int(the_sum_of_current_purchase / magic_number) * blue_card_percent * magic_number
+
+    if 15_001 <= the_sum_of_all_purchases < 150_000:
+        bonus_for_purchase = int(the_sum_of_current_purchase / magic_number) * silver_card_percent * magic_number
+
+    if the_sum_of_all_purchases >= 150_000:
+        bonus_for_purchase = int(the_sum_of_current_purchase / magic_number) * gold_card_percent * magic_number
+
+    return bonus_for_purchase
+
+print(calculate_bonuses(150_000))
+print(calculate_bonuses(15_000))
+print(calculate_bonuses(1000))
+print(calculate_bonuses(900))
---
+++ app/main.py
@@ -0,0 +1,6 @@
+from calc_bonus import calculate_bonuses
+
+the_sum_of_current_purchase = float(input('Enter the sum of current purchase: '))
+
+
+print('accrued bonuses: ', calculate_bonuses(the_sum_of_current_purchase))
